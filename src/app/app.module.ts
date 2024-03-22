ae627bccaa05b65740ab200dd246ac6383bb8b26
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-03-22 11:31:29 +0530

    Add: top and Sidebar addeed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatPanelComponent } from './Components/chat-panel/chat-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatPanelComponent
   ],
   imports: [
     BrowserModule,
